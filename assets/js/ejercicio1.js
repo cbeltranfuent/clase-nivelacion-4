1208c12188fc4fd400771b5ae5a2b95edaa91de4
cbeltranfuent <cgbf20@gmail.com>
2023-05-21 23:01:07 -0400

    acutualizo fork de mi rama en vsc

--- assets/js/ejercicio1.js
+++ assets/js/ejercicio1.js
@@ -21,24 +21,98 @@
 //     '#FFEBCD'  // Almendra claro
 // ];
 const tasks = [
-    '<p>1. Agrega un evento con el método "addEventListener" <br> que permita que al posicionar el cursor sobre cualquier elemento de la lista de clases <br> "haciaejercicio" cambie el color de fondo de este a un color aleatorio</p>',
-    '<p>2. El color debe quedar momentaneamente, de manera que quede seleccionado </p>',
-    '<p>3. Al estar todos los elementos seleccionados se habilitará el botón al siguiente ejercicio "ejercicio2.html"</p>',
+  '<p>1. Agrega un evento con el método "addEventListener" <br> que permita que al posicionar el cursor sobre cualquier elemento de la lista de clases <br> "haciaejercicio" cambie el color de fondo de este a un color aleatorio</p>',
+  '<p>2. El color debe quedar momentaneamente, de manera que quede seleccionado </p>',
+  '<p>3. Al estar todos los elementos seleccionados se habilitará el botón al siguiente ejercicio "ejercicio2.html"</p>',
 ];
+
 
 let index = 0;
 const content_container = document.getElementById('content_container');
-const changeText = (index) =>{
-    content_container.innerHTML = tasks[index];
+const changeText = (index) => {
+  content_container.innerHTML = tasks[index];
 }
-content_container.addEventListener('mouseenter', ()=>{
-    content_container.addEventListener('click', function(){
-        index == 0 ? index = 1 : index++
-        index == 3 ? index = 0:'';
-        changeText(index);
-    });
+content_container.addEventListener('mouseenter', () => {
+  content_container.addEventListener('click', function () {
+    index == 0 ? index = 1 : index++
+    index == 3 ? index = 0 : '';
+    changeText(index);
+  });
 });
-content_container.addEventListener('mouseleave', ()=>{
-    index = 0;
+content_container.addEventListener('mouseleave', () => {
+  index = 0;
 });
 
+console.log("===============resolviendo primer test========================================")
+
+//=======================================================
+// // Selecciona todos los elementos con la clase 'haciaejercicio'
+// const elements = document.querySelectorAll('.haciaejercicio');
+
+// // Función para generar un color aleatorio en formato RGB
+// const getRandomColor = () => {
+//   const r = Math.floor(Math.random() * 256);
+//   const g = Math.floor(Math.random() * 256);
+//   const b = Math.floor(Math.random() * 256);
+//   return `rgb(${r}, ${g}, ${b})`;
+// }
+
+// // Agrega un evento de 'mouseenter' y 'mouseout' a cada elemento
+// elements.forEach(element => {
+//   // Genera un color aleatorio para el elemento
+//   // const randomColor = getRandomColor();
+
+
+//   element.addEventListener('mouseover', () => {
+//     // Cambia el color de fondo del elemento al color aleatorio generado anteriormente
+//     element.style.backgroundColor = getRandomColor;
+//   });
+
+//   element.addEventListener('mouseout', () => {
+//     // Restablece el color de fondo del elemento a su valor original
+//     element.style.backgroundColor = '';
+//   });
+// });
+
+
+// //se declara boton siguiente, para acceder a "ejercicio2.html"
+// const button_next = document.getElementById('button-next');
+
+// button_next.addEventListener('click', () => {
+//   window.location.href = 'ejercicio2.html';
+// });
+
+//============================================================================================
+// Selecciona todos los elementos con la clase 'haciaejercicio'
+const elements = document.querySelectorAll('.haciaejercicio');
+
+// Función para generar un color aleatorio en formato RGB
+const getRandomColor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+// Agrega un evento de 'mouseenter' a cada elemento
+elements.forEach(element => {
+  element.addEventListener('mouseover', () => {
+    // Cambia el color de fondo del elemento a un color aleatorio
+    element.style.backgroundColor = getRandomColor();
+  });
+
+  element.addEventListener('mouseout', () => {
+    // Restablece el color de fondo del elemento a su valor original
+    element.style.backgroundColor = '';
+  });
+
+});
+
+
+
+//se declara boton siguiente, para acceder a "ejercicio2.html"
+const button_next = document.getElementById('button-next');
+
+button_next.addEventListener('click', () => {
+  window.location.href = 'ejercicio2.html';
+});
--- assets/js/ejercicio2.js
+++ assets/js/ejercicio2.js
@@ -1,11 +1,30 @@
 const ventas = [
-    { producto: 'Camisa verde', tipo:'Camisa', valor: 40000 },
-    { producto: 'Pantalón Gris',tipo:'Jeans', valor: 35000 },
-    { producto: 'Zapatos blancos',tipo:'Calzado', valor: 122000 },
-    { producto: 'Sombrero Rojo',tipo:'Sombrero', valor: 11900 },
-    { producto: 'Zapatos Amarillos',tipo:'Calzado', valor: 22900 },
-    { producto: 'Sombrero',tipo:'Sombrero', valor: 12220 },
-    { producto: 'Pantalón Negro',tipo:'Jeans', valor: 11190 },
-    { producto: 'Sombrero Violeta',tipo:'Sombrero', valor: 33390 },
-    { producto: 'Sombrero Pequeño',tipo:'Sombrero', valor: 8290 },
-  ];
+  { producto: 'Camisa verde', tipo: 'Camisa', valor: 40000 },
+  { producto: 'Pantalón Gris', tipo: 'Jeans', valor: 35000 },
+  { producto: 'Zapatos blancos', tipo: 'Calzado', valor: 122000 },
+  { producto: 'Sombrero Rojo', tipo: 'Sombrero', valor: 11900 },
+  { producto: 'Zapatos Amarillos', tipo: 'Calzado', valor: 22900 },
+  { producto: 'Sombrero', tipo: 'Sombrero', valor: 12220 },
+  { producto: 'Pantalón Negro', tipo: 'Jeans', valor: 11190 },
+  { producto: 'Sombrero Violeta', tipo: 'Sombrero', valor: 33390 },
+  { producto: 'Sombrero Pequeño', tipo: 'Sombrero', valor: 8290 },
+];
+
+/*
+calculateTotalSales(ventas): Esta función debe calcular y devolver el total de ventas realizadas. Recibe el array de ventas como parámetro.
+*/
+
+function calculateTotalSales(ventas) {
+  let total = 0;
+  for (let venta of ventas) {
+    total += venta.valor;
+  }
+  return total;
+}
+
+const totalSales = calculateTotalSales(ventas);
+console.log(totalSales);
+
+
+// =============================================================
+
